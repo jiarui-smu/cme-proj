c2bb8d2a5c1f7f5d28acebef9b378d0ded1b65d3
belly <111944572+mlem-mlem@users.noreply.github.com>
2023-10-30 21:53:12 +0800

    Create settings.py

---
+++ Project/Files/settings.py
@@ -0,0 +1,14 @@
+from os.path import join, dirname
+from dotenv import load_dotenv
+import os
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+USER = os.environ.get("MYSQL_USER")
+DATABASE_PASSWORD = os.environ.get("MYSQL_PASSWORD")
+PORT = str(os.environ.get("MYSQL_PORT"))
+MAC_USER = USER+":"+DATABASE_PASSWORD
+MAC_PORT = str(os.environ.get("MYSQL_MAC_PORT"))
+
+# gets env variables from .env file  for local machine testing
