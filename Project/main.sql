8e96a55977c3f399879c454f09421730ebf300d7
belly <111944572+mlem-mlem@users.noreply.github.com>
2023-11-03 03:35:04 +0800

    Update main.sql

--- Project/main.sql
+++ Project/main.sql
@@ -29,8 +29,8 @@ USE quikkarts;
 -- Table structure for table `customer`
 --
 
-DROP TABLE IF EXISTS CUSTOMER;
-CREATE TABLE IF NOT EXISTS CUSTOMER ( 
+DROP TABLE IF EXISTS `customer`;
+CREATE TABLE IF NOT EXISTS `customer` ( 
   `customerEmail` VARCHAR(255) NOT NULL PRIMARY KEY,
   `phoneNum` VARCHAR(20) NOT NULL,
   `bankNum` VARCHAR(20) NOT NULL,
@@ -41,7 +41,7 @@ CREATE TABLE IF NOT EXISTS CUSTOMER (
 -- Dumping data for table `clothes`
 --
 
-INSERT INTO CUSTOMER (customerEmail, phoneNum, bankNum, homeAddress)
+INSERT INTO `customer` (customerEmail, phoneNum, bankNum, homeAddress)
 VALUES
 ('john.doe@example.com', '87880300', '1234567890123456', '123 Main St.'),
 ('jane.smith@example.com', '97986025', '9876543210987654', '456 Elm St.'),
@@ -50,8 +50,8 @@ VALUES
 
 
 
-DROP TABLE IF EXISTS CLOTHES;
-CREATE TABLE IF NOT EXISTS CLOTHES (
+DROP TABLE IF EXISTS `clothes`;
+CREATE TABLE IF NOT EXISTS `clothes` (
   `clothesID` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `name` varchar(80) NOT NULL,
   `cost` decimal(10,2) NOT NULL,
@@ -61,6 +61,9 @@ CREATE TABLE IF NOT EXISTS CLOTHES (
   `image` varchar(4000) NOT NULL,
   `isOnPromotion` BOOLEAN NOT NULL
   );
+CREATE INDEX idx_clothes_name ON clothes(name);
+CREATE INDEX idx_clothes_size ON clothes(size);
+CREATE INDEX idx_clothes_color ON clothes(color);
 
 --
 -- Dumping data for table `clothes`
@@ -86,8 +89,8 @@ INSERT INTO `clothes` (`size`, `name`, `description`, `color`, `cost`, `image`, 
 
 
 
-DROP TABLE IF EXISTS ORDER1; 
-CREATE TABLE IF NOT EXISTS ORDER1 (
+DROP TABLE IF EXISTS `order1`; 
+CREATE TABLE IF NOT EXISTS `order1` (
   `order_id` int(255) NOT NULL,
   `customerEmail` varchar(32) NOT NULL,
   `clothesID` int(255) NOT NULL,
@@ -98,19 +101,20 @@ CREATE TABLE IF NOT EXISTS ORDER1 (
   `totalPrice` decimal(10,2) NOT NULL,
   `refundStatus` boolean NOT NULL,
   `transactionID` int(255) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  FOREIGN KEY (`customerEmail`) REFERENCES CUSTOMER(`customerEmail`),
-  FOREIGN KEY (`clothesID`) REFERENCES CLOTHES(`clothesID`),
-  FOREIGN KEY (`size`) REFERENCES CLOTHES(`size`),
-  FOREIGN KEY (`color`) REFERENCES CLOTHES(`color`),
-  FOREIGN KEY (`clothesName`) REFERENCES CLOTHES(`name`)
+  FOREIGN KEY (`customerEmail`) REFERENCES `customer`(`customerEmail`),
+  FOREIGN KEY (`clothesID`) REFERENCES `clothes`(`clothesID`),
+  FOREIGN KEY (`size`) REFERENCES `clothes`(`size`),
+  FOREIGN KEY (`color`) REFERENCES `clothes`(`color`),
+  FOREIGN KEY (`clothesName`) REFERENCES `clothes`(`name`)
+  
 ); 
-
+CREATE INDEX idx_order_id ON order1(order_id);
 --
 -- Dumping data for table `order`
 --
 
 INSERT INTO `order1` (`order_id`, `customerEmail`, `clothesID`, `clothesName`, `size`, `color`, `quantity`, `totalPrice`, `refundStatus`) VALUES
-(0, 'test@gmail.com', 0, 'test', 's', 'test', 1, 1.0, False);
+(1, 'ahkao@gmail.com', 1, 'Blue Shirt', 'S', 'Blue', 1, 30, False);
 
 
 
@@ -123,9 +127,8 @@ CREATE TABLE IF NOT EXISTS `invoice` (
   `totalPrice` decimal(10,2) NOT NULL,
   `payMethod` VARCHAR(100) NOT NULL,
   `stripe_id` varchar(500) NOT NULL,
-  FOREIGN KEY (`customerEmail`) REFERENCES CUSTOMER(`customerEmail`),
-  FOREIGN KEY (`order_id`) REFERENCES ORDER1(`order_id`),
-  FOREIGN KEY (`totalPrice`) REFERENCES ORDER1(`totalPrice`)
+  FOREIGN KEY (`customerEmail`) REFERENCES `customer`(`customerEmail`),
+  FOREIGN KEY (`order_id`) REFERENCES `order1` (`order_id`)
 ); 
 
 
